ebdc4eb729bbe67d01c58f382cc6169d262fb8c9
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-02-15 13:54:37 +0100

    Initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,33 @@
+import express, { Express, Request, Response } from 'express'
+import * as dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
+import connectDB from './mongodb/connect'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 5000
+const MONGODB_URL = process.env.MONGODB_URL as string
+
+const app: Express = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/', (_: Request, res: Response) => {
+  res.send({ message: 'Hello' })
+})
+
+const startServer = async () => {
+  try {
+    connectDB(MONGODB_URL)
+
+    app.listen(PORT, () =>
+      console.log(`Server started on port http://localhost:${PORT}`)
+    )
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+startServer()
